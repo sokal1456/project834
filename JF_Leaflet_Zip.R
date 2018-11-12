950e8b5468d4420d047236a1b6d9478f6a576689
jtballer4 <jtballer4@gmail.com>
2018-11-12 11:39:44 -0500

    JF with Changes

--- JF_Leaflet_Zip.R
+++ JF_Leaflet_Zip.R
@@ -18,6 +18,7 @@ library(FinCal)
 library(viridis)
 library(stringr)
 library(sf)
+library(shiny)
 
 census_clean_data_2017_bed <- get_acs(geography = "zip code tabulation area",
                                       variables = c("1" = "B25031_003", "2" = "B25031_004", "3" = "B25031_005", "4" = "B25031_006", "5" = "B25031_007"),
@@ -39,13 +40,16 @@ mutate(beds=as.numeric(beds)) %>%
 inner_join(points_by_zip,by=c("postal_code","beds")) %>%
 mutate(annualized_income=estimate*12) %>% 
 mutate(price_rent_ratio=med_price/annualized_income) %>%
-filter(state=="CT") %>%
+#filter(state=="CT") %>%
 filter(beds==2)
 
 
 #map_data$med_price<-ifelse(map_data$med_price=="NA", 0, map_data$med_price)
 pal <- colorQuantile(palette = "viridis", domain = myJoin$price_rent_ratio, n=10)
 myJoin$label<-paste0(myJoin$city, " ", (round(myJoin$price_rent_ratio,2)))
+
+
+
 
 
 myJoin %>%
@@ -66,3 +70,50 @@ addLegend("topleft",
             values = ~ med_price,
             title = "Median Housing Prices",
             opacity = .5)
+
+ui <- fluidPage(
+  leafletOutput("mymap"),
+  p(),
+  actionButton("recalc", "New points"),
+  selectInput("state", "Choose State", c("Massachusetts" = "MA", "Connecticut" = "CT")),
+    selectInput("Selector", "Choose Map Type", c("HeatMap" = "hm", "Props" = "p"))
+
+  )
+
+server <- function(input, output, session) {
+  
+output$mymap <- renderLeaflet({
+
+if (input$Selector == "hm")
+{
+  #map_data$med_price<-ifelse(map_data$med_price=="NA", 0, map_data$med_price)
+  pal <- colorQuantile(palette = "viridis", domain = myJoin$price_rent_ratio, n=10)
+  myJoin$label<-paste0(myJoin$city, " ", (round(myJoin$price_rent_ratio,2)))
+        
+    myJoin %>%
+    subset(state == input$state) %>%
+      st_transform(crs = "+init=epsg:4326") %>%
+      ### this graph shows us where the most expensive prices are
+      leaflet(width = "100%") %>%
+      addTiles() %>% 
+      addProviderTiles("CartoDB.Positron") %>% 
+      addPolygons(popup = ~ label,
+                  stroke = FALSE,
+                  smoothFactor = 0,
+                  fillOpacity = 0.7,
+                  color = ~ pal(price_rent_ratio))  
+}
+else {
+  mlsDataShow <- subset(mlsdata, state == input$state)
+  mlsDataShow$longitude <- as.double(mlsDataShow$longitude)
+  mlsDataShow$latitude <- as.double(mlsDataShow$latitude)
+    leaflet("mymap", data = mlsDataShow) %>%
+    clearMarkers() %>%
+    clearMarkerClusters() %>%
+    addTiles() %>%
+    addMarkers(~longitude, ~latitude,  clusterOptions = markerClusterOptions())  
+}
+  })
+}
+
+shinyApp(ui, server)
